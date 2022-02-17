d3ed2a16840b56c551816a87886002042f96371d
luctst <lucas.tostee@gmail.com>
2022-02-17 18:54:00 +0100

    The perfect match v2

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,88 @@
 <template>
   <div id="app">
-    <section class="content">
-      <p>{{ slideContent }}</p>
-      <svg
-        @click="goToContact"
-        version="1.1"
-        id="Layer_1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        x="0px"
-        y="0px"
-        viewBox="0 0 330 330"
-        style="enable-background: new 0 0 330 330"
-        xml:space="preserve"
-        fill="#eee"
-      >
-        <path
-          id="XMLID_24_"
-          d="M216.358,271.76c-2.322-5.605-7.792-9.26-13.858-9.26H180V15c0-8.284-6.716-15-15-15  c-8.284,0-15,6.716-15,15v247.5h-22.5c-6.067,0-11.537,3.655-13.858,9.26c-2.321,5.605-1.038,12.057,3.252,16.347l37.5,37.5  C157.322,328.536,161.161,330,165,330s7.678-1.464,10.607-4.394l37.5-37.5C217.396,283.816,218.68,277.365,216.358,271.76z"
-        />
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-        <g></g>
-      </svg>
+    <section v-if="showLoader" class="loader">
+      <h1 class="focus-in-expand">Alexandra Sanchez</h1>
     </section>
-    <section class="dark--layout"></section>
-    <carousel
-      ref="carousel"
-      :autoplay="true"
-      :perPage="1"
-      :autoplayTimeout="5000"
-      :autoplayHoverPause="false"
-      :paginationEnabled="false"
-    >
-      <slide v-for="(data, key) in 8" :key="key">
-        <img :src="require(`./assets/${data}.jpg`)" />
-      </slide>
-    </carousel>
-    <section class="contact container">
-      <header>
-        <p>Wedding planner in Paris<br/> and Bordeaux</p>
-        <div>
-          <img src="" alt="the Perfect Match logo">
-        </div>
-        <h1>Get in <br/>Touch</h1>
-      </header>
-      <main>
-        <section class="main--title">
-          <h1>Get in <br/>Touch</h1>
-        </section>
-        <section class="main--contact">
-          <div>
-            <p>EMAIL</p>
-            <a href="mailto:theperfectmath.contact@gmail.com">theperfectmath.contact@gmail.com</a>
-          </div>
-          <div>
-            <p>Perfect Match Office</p>
-            <a href="tel:0651791508">0651791508</a>
-          </div>
-      <button data-tf-slider="IhgEUzrB" data-tf-width="550" data-tf-iframe-props="title=Registration Form" style="all:unset;font-family:Helvetica,Arial,sans-serif;display:inline-block;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;background-color:#0445AF;color:#FFFFFF;font-size:20px;border-radius:25px;padding:0 33px;font-weight:bold;height:50px;cursor:pointer;line-height:50px;text-align:center;margin:0;text-decoration:none;">Launch me</button><script src="//embed.typeform.com/next/embed.js" type="application/javascript"></script>
-        </section>
-      </main>
+    <section v-else class="content--wrapper">
+      <section class="content fade-in">
+        <p>{{ slideContent }}</p>
+        <svg
+          @click="goToContact"
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 330 330"
+          style="enable-background: new 0 0 330 330"
+          xml:space="preserve"
+          fill="#eee"
+        >
+          <path
+            id="XMLID_24_"
+            d="M216.358,271.76c-2.322-5.605-7.792-9.26-13.858-9.26H180V15c0-8.284-6.716-15-15-15  c-8.284,0-15,6.716-15,15v247.5h-22.5c-6.067,0-11.537,3.655-13.858,9.26c-2.321,5.605-1.038,12.057,3.252,16.347l37.5,37.5  C157.322,328.536,161.161,330,165,330s7.678-1.464,10.607-4.394l37.5-37.5C217.396,283.816,218.68,277.365,216.358,271.76z"
+          />
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+        </svg>
+      </section>
+      <section class="dark--layout"></section>
+      <carousel
+        ref="carousel"
+        :autoplay="true"
+        :perPage="1"
+        :autoplayTimeout="3000"
+        :autoplayHoverPause="false"
+        :paginationEnabled="false"
+      >
+        <slide v-for="(data, key) in 8" :key="key">
+          <img :src="require(`./assets/${data}.jpg`)" />
+        </slide>
+      </carousel>
+      <section class="contact container">
+        <header>
+          <p>
+            Event Planner
+            <span>Paris - Bordeaux</span>
+          </p>
+          <h1>Get in <br />Touch</h1>
+        </header>
+        <main>
+          <section class="main--title">
+            <h1>Get in <br />Touch</h1>
+          </section>
+          <section class="main--contact">
+            <div class="main--contact--wrapper">
+              <div>
+                <p>EMAIL</p>
+                <a href="mailto:theperfectmath.contact@gmail.com"
+                  >theperfectmath.contact@gmail.com</a
+                >
+              </div>
+              <div>
+                <p>Perfect Match Office</p>
+                <a href="tel:0651791508">06 51 79 15 08</a>
+              </div>
+              <button data-tf-slider="FvhRquyK" data-tf-width="550" data-tf-iframe-props="title=Wedding RSVP (copy)" data-tf-medium="snippet" style="all:unset;font-family:Helvetica,Arial,sans-serif;display:inline-block;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;background-color:#877964;color:#FFFFFF;font-size:17px;border-radius:7px;padding:0 28px;font-weight:bold;height:42.5px;cursor:pointer;line-height:42.5px;text-align:center;margin:0;text-decoration:none;">Tell me more </button><script src="//embed.typeform.com/next/embed.js" type="application/javascript"></script>
+            </div>
+            <script src="//embed.typeform.com/next/embed.js" type="application/javascript"></script>
+          </section>
+        </main>
+      </section>
     </section>
   </div>
 </template>
@@ -89,10 +99,27 @@ export default {
   data() {
     return {
       slideContent: 'The Perfect Match is coming soon',
+      showLoader: true,
     };
   },
   mounted() {
+    document.querySelector('.focus-in-expand').addEventListener('animationend', () => {
+      this.showLoader = false;
+    });
     document.querySelector('body').style = 'margin: 0;padding: 0;';
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= document.querySelector('.contact').clientHeight) {
+        if (document.querySelector('button').classList.contains('shake-horizontal')) return false;
+        document.querySelector('button').classList.add('shake-horizontal');
+        return true;
+      }
+
+      if (document.querySelector('button').classList.contains('shake-horizontal')) {
+        document.querySelector('button').classList.remove('shake-horizontal');
+        return true;
+      }
+      return true;
+    });
   },
   methods: {
     goToContact() {
@@ -117,258 +144,303 @@ export default {
     margin: 0 auto;
   }
 
-  .content {
+  .loader {
+    @keyframes focus-in-expand{0%{letter-spacing:-.5em;filter:blur(12px);opacity:0}100%{filter:blur(0);opacity:1}}
+
+    .focus-in-expand{animation:focus-in-expand 3s cubic-bezier(.25,.46,.45,.94) both}
+
     align-items: center;
     display: flex;
-    flex-direction: column;
-    position: absolute;
-    height: inherit;
-    width: 100vw;
-    z-index: 11;
-
-    p {
-      font-family: 'Playfair Display', serif;
-      color: #eee;
-      font-size: 3rem;
-      margin: 0;
-      padding: 0;
-      opacity: 0.8;
-      margin: auto 0;
-      text-align: center;
-    }
-
-    svg {
-      height: 30px;
-      width: 30px;
-      margin-bottom: 8px;
-      animation-name: bounce-2;
-      animation-timing-function: ease;
-      animation-duration: 2s;
-      animation-iteration-count: infinite;
-
-      &:hover {
-        cursor: pointer;
-      }
-    }
-
-    @keyframes bounce-2 {
-      0%   { transform: translateY(0); }
-      50%  { transform: translateY(-30px); }
-      100% { transform: translateY(0); }
-    }
-  }
-
-  .dark--layout {
-    background-color: #141313;
-    opacity: 0.8;
-    position: absolute;
-    top: 0;
-    height: inherit;
-    width: 100%;
-    z-index: 10;
-  }
-
-  img {
+    justify-content: center;
     height: 100vh;
-    width: 100%;
-    max-height: 100%;
-    max-width: 100%;
-    object-fit: cover;
+    background: #f8f7f4;
+
+    h1 {
+      color: #877964;
+      font-size: 3em;
+      font-family: "Playfair Display", serif;
+    }
   }
 
-  .contact {
-    background-color: #161616;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  .content--wrapper {
+    @keyframes fade-in{0%{opacity:0}100%{opacity:1}}
+    .fade-in{animation:fade-in 4s cubic-bezier(.39,.575,.565,1.000) both}
 
-    header {
-      display: flex;
-      flex-wrap: wrap;
+      .content {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        position: absolute;
+        height: 100vh;
+        width: 100vw;
+        z-index: 11;
 
-      p {
-        margin: 0;
-        font-weight: 700;
-        color: hsla(0,0%,100%,.3);
-        text-transform: uppercase;
-        letter-spacing: .01em;
-      }
-
-      div {
-        img {
+        p {
+          font-family: "Playfair Display", serif;
           color: #fff;
-          max-height: 100%;
-          height: auto;
-          max-width: 100%;
-          object-fit: contain;
+          font-size: 3rem;
+          margin: 0;
+          padding: 0;
+          opacity: 0.8;
+          margin: auto 0;
+          text-align: center;
+        }
+
+        svg {
+          height: 30px;
+          width: 30px;
+          margin-bottom: 8px;
+          animation-name: bounce-2;
+          animation-timing-function: ease;
+          animation-duration: 2s;
+          animation-iteration-count: infinite;
+
+          &:hover {
+            cursor: pointer;
+          }
+        }
+
+        @keyframes bounce-2 {
+          0% {
+            transform: translateY(0);
+          }
+          50% {
+            transform: translateY(-30px);
+          }
+          100% {
+            transform: translateY(0);
+          }
         }
       }
 
-      h1 {
-        margin: 0;
-        color: #fff;
-        font-family: 'Playfair Display', serif;
-        font-weight: 300;
+      .dark--layout {
+        background-color: #141313;
+        opacity: 0.3;
+        position: absolute;
+        top: 0;
+        height: 100vh;
+        width: 100%;
+        z-index: 10;
       }
 
-      @media screen and (max-width: 576px) {
-        padding-top: 16px;
+      img {
+        height: 100vh;
+        width: 100%;
+        max-height: 100%;
+        max-width: 100%;
+        object-fit: cover;
+      }
+
+      .contact {
+        background-color: #f8f7f4;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
         justify-content: space-between;
 
-        p {
-          font-size: .6rem;
-        }
-
-        h1 {
-          display: block;
-          font-size: 4.6rem;
-          margin-top: 16px;
-          flex: 0 0 100%;
-        }
-      }
-
-      @media screen and (min-width: 577px) {
-        padding-top: 16px;
-
-        p {
-          font-size: .6rem;
-        }
-
-        div {
-          margin: 0 auto;
-        }
-
-        h1 {
-          flex: 0 0 100%;
-          font-size: 4.6rem;
-          margin-top: 16px;
-        }
-      }
-
-      @media screen and (min-width: 700px) {
-        h1 {
-          font-size: 5.6rem;
-        }
-      }
-
-      @media screen and (min-width: 800px) {
-        h1 {
-          display: none;
-        }
-      }
-    }
-
-    main {
-      display: flex;
-
-      .main--contact {
-        width: 100%;
-
-        div {
-          margin-bottom: 25px;
+        header {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
 
           p {
-            color: hsla(0,0%,100%,.3);
-            font-weight: 700;
-            margin-bottom: .9rem;
+            font-family: "Playfair Display", serif;
+            text-align: center;
             margin: 0;
-            text-transform: uppercase;
-          }
-
-          a {
             font-weight: 700;
-            text-decoration: none;
+            color: #877964;
             text-transform: uppercase;
-            color: #fff;
+            letter-spacing: 0.01em;
+
+            span {
+              display: block;
+              font-size: .7em;
+            }
           }
-        }
-      }
 
-      .main--title {
-        button {
-          height: 4.58333vw;
-          width: 4.58333vw;
-        }
-      }
-
-      @media screen and (max-width: 576px) {
-        .main--title {
           h1 {
-            display: none;
-          }
-        }
-        .main--contact {
-          text-align: center;
-
-          div {
-            p {
-              font-size: 14px;
-              margin-bottom: 8px;
-            }
-            a {
-              font-size: 14px;
-            }
-          }
-        }
-      }
-
-      @media screen and (min-width: 577px) {
-        .main--title {
-          h1 {
-            display: none;
-          }
-        }
-
-        .main--contact {
-          text-align: center;
-
-          div {
-            p {
-              font-size: 14px;
-              margin-bottom: 8px;
-            }
-            a {
-              font-size: 14px;
-            }
-          }
-        }
-      }
-
-      @media screen and (min-width: 800px) {
-        display: flex;
-
-        .main--title {
-          h1 {
-            color: #fff;
-            display: block;
-            font-family: 'Playfair Display', serif;
-            font-size: 9.5rem;
             margin: 0;
-            letter-spacing: -.03em;
+            color: #877964;
+            font-family: "Playfair Display", serif;
+            font-weight: 300;
+          }
+
+          @media screen and (max-width: 576px) {
+            padding-top: 16px;
+            justify-content: space-between;
+
+            p {
+              font-size: 1.6rem;
+            }
+
+            h1 {
+              display: block;
+              font-size: 4.6rem;
+              margin-top: 16px;
+              flex: 0 0 100%;
+            }
+          }
+
+          @media screen and (min-width: 577px) {
+            padding-top: 16px;
+
+            p {
+              font-size: 1.6rem;
+            }
+
+            div {
+              margin: 0 auto;
+            }
+
+            h1 {
+              flex: 0 0 100%;
+              font-size: 4.6rem;
+              margin-top: 16px;
+            }
+          }
+
+          @media screen and (min-width: 700px) {
+            h1 {
+              font-size: 5.6rem;
+            }
+          }
+
+          @media screen and (min-width: 800px) {
+            h1 {
+              display: none;
+            }
           }
         }
 
-        .main--contact {
-          text-align: left;
-          margin-top: 80px;
-          margin-left: 32px;
-        }
-      }
+        main {
+          display: flex;
 
-      @media screen and (min-width: 1000px) {
-        .main--title {
-          h1 {
-            font-size: 13.6rem;
+          .main--contact {
+            @keyframes shake-horizontal{0%,100%{transform:translateX(0)}10%,30%,50%,70%{transform:translateX(-10px)}20%,40%,60%{transform:translateX(10px)}80%{transform:translateX(8px)}90%{transform:translateX(-8px)}}
+
+            .shake-horizontal{
+              animation:shake-horizontal .8s cubic-bezier(.455,.03,.515,.955) both!important;
+            }
+
+            display: flex;
+            justify-content: center;
+            font-family: "Playfair Display", serif;
+            align-items: flex-end;
+            width: 100%;
+
+            &--wrapper {
+              margin-top: 32px;
+              display: flex;
+              flex-direction: column;
+
+              button {
+                width: 50%!important;
+              }
+            }
+
+            div {
+              margin-bottom: 32px;
+
+              p {
+                color: #877964;
+                font-weight: 700;
+                margin-bottom: 0.9rem;
+                margin: 0;
+                text-transform: uppercase;
+              }
+
+              a {
+                text-decoration: none;
+                color: #83745d;
+              }
+            }
           }
-        }
 
-        .main--contact {
-          margin-top: 80px;
-          margin-left: 80px;
+          .main--title {
+            color: #877964;
+
+            button {
+              height: 4.58333vw;
+              width: 4.58333vw;
+            }
+          }
+
+          @media screen and (max-width: 576px) {
+            .main--title {
+              h1 {
+                display: none;
+              }
+            }
+            .main--contact {
+              text-align: center;
+
+              div {
+                p {
+                  font-size: 14px;
+                  // margin-bottom: 8px;
+                }
+                a {
+                  font-size: 14px;
+                }
+              }
+            }
+          }
+
+          @media screen and (min-width: 577px) {
+            .main--title {
+              h1 {
+                display: none;
+              }
+            }
+
+            .main--contact {
+              text-align: center;
+
+              div {
+                p {
+                  font-size: 14px;
+                  // margin-bottom: 8px;
+                }
+                a {
+                  font-size: 14px;
+                }
+              }
+            }
+          }
+
+          @media screen and (min-width: 800px) {
+            display: flex;
+
+            .main--title {
+              h1 {
+                color: #877964;
+                display: block;
+                font-family: "Playfair Display", serif;
+                font-size: 9.5rem;
+                margin: 0;
+                letter-spacing: -0.03em;
+              }
+            }
+
+            .main--contact {
+              text-align: left;
+              margin-top: 80px;
+            }
+          }
+
+          @media screen and (min-width: 1000px) {
+            .main--title {
+              h1 {
+                font-size: 13.6rem;
+              }
+            }
+
+            .main--contact {
+              margin-top: 80px;
+            }
+          }
         }
       }
     }
   }
-}
 </style>
