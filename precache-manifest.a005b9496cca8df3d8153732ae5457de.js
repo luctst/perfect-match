d447e7cbc4791d75ae4bedc711e5f8767e532350
luctst <lucas.tostee@gmail.com>
2022-02-17 19:46:31 +0100

    deploy

---
+++ precache-manifest.a005b9496cca8df3d8153732ae5457de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a6f52e2c60bdaefa9f",
+    "url": "/css/app.bc51e8e6.css"
+  },
+  {
+    "revision": "a4c46f70152ae621ce02e285cab8d1fd",
+    "url": "/img/1.a4c46f70.jpg"
+  },
+  {
+    "revision": "4329aa1866a6071f0096ba4a190acab7",
+    "url": "/img/2.4329aa18.jpg"
+  },
+  {
+    "revision": "c584b9cc7ea1944dcb0f28cab6a495ee",
+    "url": "/img/3.c584b9cc.jpg"
+  },
+  {
+    "revision": "9acf9efe4ecd79d69fed20d0c61a9b36",
+    "url": "/img/4.9acf9efe.jpg"
+  },
+  {
+    "revision": "e5a9613945ca9037230521e4e4813d82",
+    "url": "/img/5.e5a96139.jpg"
+  },
+  {
+    "revision": "c0e0a5dff1c24181fbd44f4f8a086800",
+    "url": "/img/6.c0e0a5df.jpg"
+  },
+  {
+    "revision": "4adf6d7fc424c695de3aed20ef5d6447",
+    "url": "/img/7.4adf6d7f.jpg"
+  },
+  {
+    "revision": "74b61c83625725a50d1136d92f580038",
+    "url": "/img/8.74b61c83.jpg"
+  },
+  {
+    "revision": "4eec9d317cfbde70b3a2434591272bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51a6f52e2c60bdaefa9f",
+    "url": "/js/app.c13872f8.js"
+  },
+  {
+    "revision": "0cfeabb99096fdcd1461",
+    "url": "/js/chunk-vendors.17f2da8e.js"
+  },
+  {
+    "revision": "00852fc5e7c6aadac0ff185f7231b93f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
