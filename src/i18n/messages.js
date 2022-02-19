b421fe076422b3ff490255e7c0fa28c6b135ed5a
luctst <lucas.tostee@gmail.com>
2022-02-19 20:56:28 +0100

    Start adding i18n

---
+++ src/i18n/messages.js
@@ -0,0 +1,12 @@
+export default {
+  en: {
+    carouselTitle: 'The Perfect Match is coming soon',
+    footerHeader: 'Event Planner <span>Paris - Bordeaux</span>',
+    footerContent: 'Get in <br/> touch',
+  },
+  fr: {
+    carouselTitle: 'The Perfect Match arrive bientôt',
+    footerHeader: 'Organisatrice d\'évenements <span>Paris - Bordeaux</span>',
+    footerContent: 'Entrons en contact',
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import messages from './i18n/messages';
 import App from './App.vue';
 import './registerServiceWorker';
 
+Vue.use(VueI18n);
 Vue.config.productionTip = false;
 
 new Vue({
+  i18n: new VueI18n({
+    locale: navigator.language.includes('-') ? navigator.language.split('-')[0] : navigator.language,
+    messages,
+  }),
   render: (h) => h(App),
 }).$mount('#app');
